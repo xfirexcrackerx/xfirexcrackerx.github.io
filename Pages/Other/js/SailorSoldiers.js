cb138708293dc55626c4c9f0b145cf443b6ea071
Kristina <xfirexcrackerx@gmail.com>
2017-03-20 15:12:28 +0200

    Sailor Moon website
    :)

---
+++ Pages/Other/js/SailorSoldiers.js
@@ -0,0 +1,41 @@
+function topFunction1() {
+    document.body.scrollTop = 160; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction2() {
+    document.body.scrollTop = 2165; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction3() {
+    document.body.scrollTop = 3743; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction4() {
+    document.body.scrollTop = 4860; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction5() {
+    document.body.scrollTop = 6560; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction6() {
+    document.body.scrollTop = 7730; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction7() {
+    document.body.scrollTop = 8890; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction8() {
+    document.body.scrollTop = 10460; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction9() {
+    document.body.scrollTop = 11886; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+function topFunction10() {
+    document.body.scrollTop = 13500; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
